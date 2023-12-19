d414a103a70098896ae9e04e5f56a4579f13d9a5
sarawut <sarawutkai99@gmail.com>
2023-12-19 11:07:45 +0700

    Add project files.

---
+++ Tabcontrol/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Tabcontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 164);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1459, 784);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.webBrowser1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1451, 755);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1445, 749);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1325, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(34, 125);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(1222, 22);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.ImageIndex = 0;
+            this.btnBack.ImageList = this.imageList1;
+            this.btnBack.Location = new System.Drawing.Point(34, 27);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(92, 49);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            this.btnBack.MouseHover += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.ImageIndex = 1;
+            this.btnForward.ImageList = this.imageList1;
+            this.btnForward.Location = new System.Drawing.Point(132, 27);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(92, 49);
+            this.btnForward.TabIndex = 4;
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            this.btnForward.MouseHover += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btn
+            // 
+            this.btn.ImageIndex = 2;
+            this.btn.ImageList = this.imageList1;
+            this.btn.Location = new System.Drawing.Point(230, 27);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(92, 49);
+            this.btn.TabIndex = 5;
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            this.btn.MouseHover += new System.EventHandler(this.btn_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.ImageIndex = 3;
+            this.btnStop.ImageList = this.imageList1;
+            this.btnStop.Location = new System.Drawing.Point(328, 27);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(92, 49);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnStop.MouseHover += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.ImageIndex = 4;
+            this.btnRefresh.ImageList = this.imageList1;
+            this.btnRefresh.Location = new System.Drawing.Point(426, 27);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(92, 49);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.btnRefresh.MouseHover += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "4829870_arrow_back_left_icon.png");
+            this.imageList1.Images.SetKeyName(1, "4781858_arrow_arrows_back_direction_left_icon.png");
+            this.imageList1.Images.SetKeyName(2, "326656_home_icon.png");
+            this.imageList1.Images.SetKeyName(3, "9104213_close_cross_remove_delete_icon (2).png");
+            this.imageList1.Images.SetKeyName(4, "8111405_reset_reload_refresh_sync_arrow_icon (1).png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1715, 960);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Tabcontrol/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabcontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string WebPage = txtAddress.Text.Trim();
+
+            webBrowser1.Navigate(WebPage);
+            tabPage2.Text = WebPage;
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(btnBack, "Back one page");
+            if(webBrowser1.CanGoBack)
+            { 
+                webBrowser1.GoBack(); 
+            }
+        }
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(btnForward, "Forward one page");
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(btn, "Go To Home");
+            if (webBrowser1.IsBusy)
+            {
+                webBrowser1.GoHome();
+            }
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(btnStop, "Stop to Load Page");
+            if (webBrowser1.IsBusy)
+            {
+                webBrowser1.Stop();
+            }
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(btnStop, "Refresh to Page");
+            webBrowser1.Refresh();
+        }
+    }
+}
